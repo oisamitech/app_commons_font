4bf8c451be2b224851f5f0483c852267ca5bbd63
Vitor Hugo <vitor_americo@hotmail.com>
2022-03-19 09:21:50 -0300

    Adicionado class busca icones por nome

---
+++ lib/icon_util.dart
@@ -0,0 +1,38 @@
+library font_awesome_flutter;
+
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:font_awesome_flutter/name_icon_mapping.dart';
+
+class IconUtil {
+  IconUtil._();
+
+  static IconData getIconByName(String iconName, String iconType) {
+    IconData? icon;
+
+    icon = faIconNameMapping['$iconType $iconName'];
+
+    icon ??= FontAwesomeIcons.lightCircleExclamation;
+
+    return icon;
+  }
+
+  static IconData getIconById(String iconId, String iconType) {
+    var codePoint = int.parse('0x$iconId');
+
+    switch (iconType) {
+      case 'light':
+        return IconDataLight(codePoint);
+      case 'solid':
+        return IconDataSolid(codePoint);
+      case 'regular':
+        return IconDataRegular(codePoint);
+      case 'brands':
+        return IconDataBrands(codePoint);
+      case 'duotone':
+        return IconDataDuotone(codePoint);
+      default:
+        return FontAwesomeIcons.lightCircleExclamation;
+    }
+  }
+}
